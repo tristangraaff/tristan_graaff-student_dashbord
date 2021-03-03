40c4d3c572e28cccb349095fec6d50ef9bc418fc
tristangraaff <67099158+tristangraaff@users.noreply.github.com>
2021-03-03 15:19:55 +0100

    Add files via upload

---
+++ student-dashboard eerste versie/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Components/App';
+import * as V from 'victory';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
